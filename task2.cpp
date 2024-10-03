bda9b25ea95b0d4e05ac40879f71cc6233e630e9
cstdout <151808195+cstdout@users.noreply.github.com>
2024-10-04 03:44:24 +0400

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+   int gramms = -1;
+    do {
+      std::cout << "Enter the value in gramms" << std::endl;
+      std::cin >> gramms;
+      if (gramms < 0)
+      {
+            std::cout << "Value must be positive" << std::endl;
+      } 
+    } while(gramms < 0);
+    
+    float kilogramms = (float)gramms / 1000.0f;
+    std::cout << "Kilogramms: " <<  kilogramms << std::endl;
+    return 0;
+}
